a270c40264fbde47906369d8c3fcefe84f07d259
mohammadnassar4 <mohammadnassar044@gmail.com>
2025-10-22 15:39:23 +0300

    A script that prints the full description of the table books from the database alx_book_store in your MySQL server.

--- task_4.sql
+++ task_4.sql
@@ -1,4 +1,11 @@
-SELECT * FROM 
+SELECT 
+    COLUMN_NAME AS 'Field',
+    COLUMN_TYPE AS 'Type',
+    IS_NULLABLE AS 'Null',
+    COLUMN_KEY AS 'Key',
+    COLUMN_DEFAULT AS 'Default',
+    EXTRA AS 'Extra'
+FROM 
     INFORMATION_SCHEMA.COLUMNS
 WHERE 
     TABLE_SCHEMA = 'alx_book_store'
